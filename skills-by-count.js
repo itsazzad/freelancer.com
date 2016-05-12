31eef2a4bae5d6e020c8ea617ce4bc14099da75f
Sazzad Hossain Khan <itsazzad@gmail.com>
2016-05-12 19:49:06 +0600

    skills-by-count.js

---
+++ skills-by-count.js
@@ -0,0 +1,19 @@
+/*
+Run the below code in the console of https://www.freelancer.com/job/
+*/
+var skills=[];
+jQuery('.job-category-item').each(function(){
+   var title=jQuery(this).find('.job-category-link').text().trim();
+   var matches = title.match(/\(([^)]*)\)[^(]*$/);
+   console.log(matches);
+   console.log(matches.length);
+   console.log(matches[1]);
+   console.log(matches.input.substr(0, matches.index-1));
+   if(jQuery.isArray(skills[matches[1]])){
+   }else{
+      skills[matches[1]]=new Array();
+   }
+   skills[matches[1]].push(matches.input.substr(0, matches.index-1));
+
+});
+console.log(skills);
