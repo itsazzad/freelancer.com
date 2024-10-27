91c37f513e0b14c377d85bc9c52300ad4f625799
Sazzad Hossain (Tushar) Khan <itsazzad@gmail.com>
2024-10-27 11:20:31 +0600

    Update skills-by-count.js

--- skills-by-count.js
+++ skills-by-count.js
@@ -1,15 +1,32 @@
 /*
 Run the code in the console of https://www.freelancer.com/job/
 */
-var skills=[];
-jQuery('.job-category-item').each(function(){
-   var title=jQuery(this).find('.job-category-link').text().trim();
-   var matches = title.match(/\(([^)]*)\)[^(]*$/);
-   if(jQuery.isArray(skills[matches[1]])){
-   }else{
-      skills[matches[1]]=new Array();
-   }
-   skills[matches[1]].push(matches.input.substr(0, matches.index-1));
 
-});
-console.log(skills);
+{
+    let skills = {};
+    jQuery('li[data-job-category-item]').each(function () {
+        let sum = 0;
+        let title;
+        jQuery(this).find('a.PageJob-category-link').each(function (index) {
+            if (index === 0) {
+                const titleLine = jQuery(this).text().trim();
+                const matches = titleLine.match(/\(([^)]*)\)[^(]*$/);
+                title = matches.input.substr(0, matches.index - 1);
+                sum += +(matches[1]);
+            } else {
+                const titleLine = jQuery(this).text().trim();
+                const matches = titleLine.match(/\(([^)]*)\)[^(]*$/);
+                sum += +(matches[1]);
+            }
+        });
+
+        if (sum > 0) {
+            if (jQuery.isArray(skills[sum])) {
+            } else {
+                skills[sum] = new Array();
+            }
+            skills[sum].push(title);
+        }
+    });
+    console.log(skills);
+}
